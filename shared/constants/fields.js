b17cc883fb959f99bbec811b6cd73c33f7327635
Luke Hamilton <lukehamiltonmail@gmail.com>
2019-09-22 22:14:20 -0400

    shared constants

---
+++ shared/constants/fields.js
@@ -0,0 +1,53 @@
+export const FIELD_LABELS = {
+  gene: 'Gene',
+  nucleotideChange: 'Nucleotide Change',
+  proteinChange: 'Protein Change',
+  otherMappings: 'Other Mappings',
+  alias: 'Alias',
+  transcripts: 'Transcripts',
+  region: 'Region',
+  reportedClassification: 'Reported Classification',
+  inferredClassification: 'Inferred Classification',
+  source: 'Source',
+  lastEvaluated: 'Last Evaluated',
+  lastUpdated: 'Last Updated',
+  url: 'URL',
+  submitterComment: 'Submitter Comment',
+  assembly: 'Assembly',
+  chr: 'Chr',
+  genomicStart: 'Genomic Start',
+  genomicStop: 'Genomic Stop',
+  ref: 'Ref',
+  alt: 'Alt',
+  accession: 'Accession',
+  reportedRef: 'Reported Ref',
+  reportedAlt: 'Reported Alt'
+}
+
+export const HEAD_SLUGS = [
+  'gene',
+  'nucleotideChange',
+  'proteinChange',
+  'alias',
+  'region',
+  'reportedClassification',
+  'lastEvaluated',
+  'lastUpdated'
+]
+
+export const OTHER_SLUGS = [
+  'otherMappings',
+  'transcripts',
+  'inferredClassification',
+  'source',
+  'submitterComment',
+  'assembly',
+  'chr',
+  'genomicStart',
+  'genomicStop',
+  'ref',
+  'alt',
+  'accession',
+  'reportedRef',
+  'reportedAlt'
+]
