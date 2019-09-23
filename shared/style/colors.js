71e453e830af9f8d8c8c08b4a2a4d3c446e0130f
Luke Hamilton <lukehamiltonmail@gmail.com>
2019-09-22 22:14:33 -0400

    Style utilities and colors

---
+++ shared/style/colors.js
@@ -0,0 +1,7 @@
+export const NIAGRA = '#0aaca0'
+export const FOAM = '#C9EDEA'
+export const DALI = '#36BBB1'
+export const WHITE = '#FFF'
+export const ALABASTER = '#F8F8F8'
+export const RICE_CAKE = '#F3F3F3'
+export const LILAC = '#E8E8E8'
