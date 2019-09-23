d7fb8275b1546d66bfa17449f047c23941682172
Luke Hamilton <lukehamiltonmail@gmail.com>
2019-09-22 22:15:03 -0400

    Update main application structure

--- pages/index.js
+++ pages/index.js
@@ -1,112 +1,49 @@
-import React from "react";
-import Head from "next/head";
-import Nav from "../components/nav";
-import styled from "@emotion/styled";
-import { keyframes, css, Global } from "@emotion/core";
+import React, { Fragment } from 'react'
+import Head from 'next/head'
+import styled from '@emotion/styled'
+import { keyframes, css, Global } from '@emotion/core'
+import App from '../components/app'
+import { RICE_CAKE } from '../shared/style/colors'
 
-const basicStyles = css`
-  background-color: white;
-  color: cornflowerblue;
-  border: 1px solid lightgreen;
-  border-right: none;
-  border-bottom: none;
-  box-shadow: 5px 5px 0 0 lightgreen, 10px 10px 0 0 lightyellow;
-  transition: all 0.1s linear;
-  margin: 3rem 0;
-  padding: 1rem 0.5rem;
-`;
-const hoverStyles = css`
-  &:hover {
-    color: white;
-    background-color: lightgray;
-    border-color: aqua;
-    box-shadow: -15px -15px 0 0 aqua, -30px -30px 0 0 cornflowerblue;
-  }
-`;
-const bounce = keyframes`
-  from {
-    transform: scale(1.01);
-  }
-  to {
-    transform: scale(0.99);
-  }
-`;
-
-const Basic = styled.div`
-  ${basicStyles};
-`;
-
-const Combined = styled.div`
-  ${basicStyles};
-  ${hoverStyles};
-  & code {
-    background-color: linen;
-  }
-`;
-const Animated = styled.div`
-  ${basicStyles};
-  ${hoverStyles};
-  & code {
-    background-color: linen;
-  }
-  animation: ${props => props.animation} 0.2s infinite ease-in-out alternate;
-`;
+const Page = styled('div')({
+  paddingTop: 40,
+  height: '100%'
+})
 
 const Home = () => (
-  <div>
+  <Page>
+    <style></style>
     <Head>
-      <title>Home</title>
+      <title>Luke Hamilton Coding Challenge</title>
     </Head>
-
-    <Nav />
+    <Global
+      styles={{
+        '#__next': {
+          height: '100vh'
+        }
+      }}
+    />
     <Global
       styles={css`
         html,
         body {
-          padding: 3rem 1rem;
           margin: 0;
-          background: papayawhip;
+          background-color: ${RICE_CAKE};
+          height: 100%;
           min-height: 100%;
           font-family: Helvetica, Arial, sans-serif;
           font-size: 24px;
+          ' > div': {
+            display: 'none';
+          }
+        }
+        #__next {
+          height: 100%;
         }
       `}
     />
-    <Head>
-      <title>With Emotion</title>
-    </Head>
-    <div>
-      <Basic>Cool Styles</Basic>
-      <Combined>
-        With <code>:hover</code>.
-      </Combined>
-      <Animated animation={bounce}>Let's bounce.</Animated>
-    </div>
-    {/* <div className="hero">
-      <h1 className="title">Welcome to Next.js!</h1>
-      <p className="description">
-        To get started, edit <code>pages/index.js</code> and save to reload.
-      </p>
+    <App />
+  </Page>
+)
 
-      <div className="row">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Learn more about Next.js in the documentation.</p>
-        </a>
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Next.js Learn &rarr;</h3>
-          <p>Learn about Next.js by following an interactive tutorial!</p>
-        </a>
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Find other example boilerplates on the Next.js GitHub.</p>
-        </a>
-      </div>
-    </div> */}
-  </div>
-);
-
-export default Home;
+export default Home
